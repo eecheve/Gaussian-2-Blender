5f3ca47de6d2903b61dfae079a4ed9c665987983
Emmanuel Echeverri <ei.echeverri2@gmail.com>
2025-01-18 15:50:50 -0400

    1/18/25_3:49PM
    - Bond order calculator done.
    - .xyz reader done.
    To do: start from the GUI.
      - A dropdown menu to select the type of inpyt file.

---
+++ scripts/__init__.py
@@ -0,0 +1,3 @@
+# Empty file within the Scripts folder
+# this file allows Gaussian2Blender to identify the gui folder as a package
+# I'm doing this to import all the modules within gui and use them in the Gaussian2Blender.py
