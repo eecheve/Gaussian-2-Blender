748731e6d1e32dd5d420d423b4ba2febb84a6190
Emmanuel Echeverri <ei.echeverri2@gmail.com>
2025-06-30 00:05:47 -0400

    extra folder erased

--- macOS/TheorChem2Blender.app/Contents/Resources/gui/__init__.py
+++
@@ -1,3 +0,0 @@
-# Empty file within the GUI folder
-# this file allows Gaussian2Blender to identify the gui folder as a package
-# I'm doing this to import all the modules within gui and use them in the Gaussian2Blender.py
